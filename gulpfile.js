6e20d9fa53d0631c8a65055b4c313b4729eb5991
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2020-07-15 18:51:57 +0700

    added command to clean _site folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const del = require('del')
 
-gulp.task('buildcss', function() {
+gulp.task('css', function() {
     return gulp.src('./src/sass/*.scss')
     .pipe(sass({outputStyle: 'compressed'})
     .on('error', sass.logError))
     .pipe(gulp.dest('./_site/assets/css/'))
 })
 
-gulp.task('watch', function() {
-    gulp.watch('./src/sass/*.scss', gulp.parallel('buildcss'))
+gulp.task('clean', function() {
+    return del([
+        '.cache',
+        '_site'
+    ])
 })
 
-gulp.task('build', gulp.parallel('buildcss'))
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/*.scss', gulp.parallel('css'))
+})
+
+gulp.task('build', gulp.parallel('css'))
