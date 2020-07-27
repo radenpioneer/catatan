c17cb74daf428d47453316857246002e34bb967f
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2020-07-27 13:18:05 +0700

    seo optimization

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,3 +1,17 @@
+const environment = process.env.ELEVENTY_ENV
+const PROD_ENV = 'prod'
+const prodUrl = 'https://radenpioneer.xyz'
+const devUrl = 'http://localhost:8080'
+const baseUrl = environment === PROD_ENV ? prodUrl : devUrl
+const isProd = environment === PROD_ENV
+
 module.exports = {
-    environment: process.env.ELEVENTY_ENV
+    environment,
+    isProd,
+    site: {
+        title: "Sastronegoro",
+        description: "notes and thoughts",
+        logo: "/assets/images/profile.jpg",
+        url: baseUrl,
+    }
 }
