d6ac774e2cfc1576fd703ac556ee03a973bbf788
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2020-08-29 22:35:39 +0700

    change sass compilation task

--- tasks/sass.task.js
+++ tasks/sass.task.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 sass.compiler = require('sass')
+const replace = require('gulp-replace')
+
+//PostCSS and plugins
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const importcss = require('postcss-import-url')
 
 gulp.task('generatecss', function() {
+    let processors = [
+        autoprefixer,
+        importcss
+    ]
     return gulp.src('./src/sass/style.scss')
-    .pipe(sass({outputStyle: 'compressed'})
-    .on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(replace(' !important', ''))
     .pipe(gulp.dest('./src/_includes/partials/'))
 })
 
