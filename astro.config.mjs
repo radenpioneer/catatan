813633ff1c15b1db3543ba59a6eddc48c16c390f
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2022-03-09 11:34:50 +0700

    added configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 export default /** @type {import('astro').AstroUserConfig} */ {
+    buildOptions: {
+        site: 'https://radenazure86.netlify.app',
+        sitemap: false
+    },
     renderers: [
         '@astrojs/renderer-react'
     ]
